65dfb08146b8e66eac05632cd9517fabb26cc439
sauravrajleaf <saurved785@gmail.com>
2021-06-28 17:21:12 +0530

    MongoDB is connected to the backend

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express(); //INITIALIZING EXPRESS
+
+//CONNECTS DATABASE
+connectDB();
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to your Personal TODO App" })
