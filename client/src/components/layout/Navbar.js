b410f2de404b688221f9e18a051c04119be5b8a9
sauravrajleaf <saurved785@gmail.com>
2021-09-02 18:10:13 +0530

    MongoDB connection working again

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
+
+import "./Navbar.css";
+class Navbar extends Component {
+  state = { clicked: false };
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  };
+
+  render() {
+    return (
+      <div className="NavbarItems">
+        <h1 className="navbar-logo">
+          Personal TODO App <i className="far fa-calendar-check "></i>
+        </h1>
+        <div className="menu-icon" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link to={item.url} className={item.cName}>
+                  {item.title}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
